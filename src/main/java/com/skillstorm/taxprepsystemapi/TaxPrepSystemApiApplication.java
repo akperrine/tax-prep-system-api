8605b1691a5f303b6ecf53a3689d7891b8ec8452
sessums-smoothstack <dev.sessums@gmail.com>
2023-07-17 15:24:53 -0400

    initial commit

---
+++ src/main/java/com/skillstorm/taxprepsystemapi/TaxPrepSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxprepsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxPrepSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxPrepSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skillstorm/taxprepsystemapi/TaxPrepSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxprepsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxPrepSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
