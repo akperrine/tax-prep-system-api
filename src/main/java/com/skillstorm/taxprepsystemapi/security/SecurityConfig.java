51183088d44c090404cfd076e2541ef4cafd40bc
Austin Perrine <akperrine@gmail.com>
2023-08-10 17:36:56 -0700

    disable csrf

--- src/main/java/com/skillstorm/taxprepsystemapi/security/SecurityConfig.java
+++ src/main/java/com/skillstorm/taxprepsystemapi/security/SecurityConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -17,15 +20,20 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+//new
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+@CrossOrigin(allowCredentials = "true")
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                /*.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers("/register")
-                .and()*/
-                .csrf().disable()
+                .cors()
+                .and()
+                .sessionManagement(session -> session.sessionCreationPolicy((SessionCreationPolicy.STATELESS)))
                 .authorizeHttpRequests(
                         (authorize) ->
                                 authorize
@@ -34,9 +42,10 @@ public class SecurityConfig {
                                         .mvcMatchers("/user/**").permitAll()
                                         .mvcMatchers("/tax/**").permitAll()
                 )
-                .cors()
-                .and()
                 .httpBasic();
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers("/register").disable();
 
         return http.build();
     }
